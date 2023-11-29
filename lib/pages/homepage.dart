7cda4af28427c84ab29a182a593f1a4dbf8a573c
Amina_Kabir <ameenakabir22@gmail.com>
2023-11-29 20:17:08 +0000

    1

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,6 +14,50 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Call Monitoring'),
         centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(
+                Icons.settings,
+                size: 48,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle,
+              ),
+              title: const Text(' A C C O U N T '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.insert_drive_file,
+              ),
+              title: const Text('R E C O R D S'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            SizedBox(height: 25),
+            ListTile(
+              leading: Icon(
+                Icons.supervised_user_circle,
+              ),
+              title: const Text('S U P E R V I S I O N'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
